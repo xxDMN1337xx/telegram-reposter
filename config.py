326d75b44318720cbec92fc9e87cc660d9a16d51
xxDMN1337xx <youtestdp@gmail.com>
2025-04-29 21:02:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-config.py
+import os
+
+API_ID = int(os.getenv("API_ID"))  # Получи у Telegram
+API_HASH = os.getenv("API_HASH")
+SESSION_NAME = os.getenv("SESSION_NAME", "session")
+TARGET_CHANNEL = os.getenv("TARGET_CHANNEL")  # id канала, куда репостить
