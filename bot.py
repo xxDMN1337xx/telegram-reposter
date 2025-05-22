5fce53c0e83c60f16806af775cba80897ab430f9
xxDMN1337xx <youtestdp@gmail.com>
2025-05-23 00:02:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,11 +38,9 @@ def normalize_text(text):
 
 # === Провайдеры без авторизации
 fallback_providers = [
-    g4f.Provider.Chatgpt4o,      # gpt-4o (иногда дольше)
-    g4f.Provider.FreeGpt,        # gpt-3.5
-    g4f.Provider.Browserbase,    # gpt-3.5
-    g4f.Provider.OpenaiChat,     # gpt-3.5
-    g4f.Provider.Yqcloud         # gpt-4
+    g4f.Provider.Chatgpt4o,   # GPT-4o
+    g4f.Provider.FreeGpt,     # GPT-3.5
+    g4f.Provider.Yqcloud      # GPT-4
 ]
 
 
