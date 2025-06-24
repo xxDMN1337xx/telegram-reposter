1eb394113088580775ce1c5b867c585719070e1a
xxDMN1337xx <youtestdp@gmail.com>
2025-06-24 13:38:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,11 +22,12 @@ COPY_CHANNELS = {
 fallback_providers = [
     # === g4f.Provider.AnyProvider,
     g4f.Provider.Blackbox,
+    g4f.Provider.Chatai,
     g4f.Provider.CohereForAI_C4AI_Command,
-    g4f.Provider.Free2GPT,
-    g4f.Provider.Qwen_Qwen_2_5,
+    g4f.Provider.OIVSCodeSer0501,
+    g4f.Provider.OIVSCodeSer2,
+    g4f.Provider.PollinationsAI,
     g4f.Provider.Qwen_Qwen_2_5_Max,
-    g4f.Provider.Qwen_Qwen_2_72B,
     g4f.Provider.WeWordle,
     g4f.Provider.Yqcloud,
 ]
